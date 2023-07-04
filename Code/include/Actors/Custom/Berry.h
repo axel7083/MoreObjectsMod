b1f1a4e3c5448653197aa694ba701796d4783cf2
Splatterboy64 <janssensdylian@gmail.com>
2023-07-04 15:46:44 +0200

    Start refactor, port Berry

---
+++ Code/include/Actors/Custom/Berry.h
@@ -0,0 +1,35 @@
+#pragma once
+
+struct Berry : Actor
+{
+	Model model;
+	Model stem;
+	MovingCylinderClsn cylClsn;
+	ShadowModel shadow;
+	Vector3 origPos;
+	Fix12i berryOffsetX;
+	Fix12i berryOffsetZ;
+	bool groundFound;
+	bool killed;
+	bool isOnFloor;
+	
+	Berry();
+	virtual s32 InitResources() override;
+	virtual s32 CleanupResources() override;
+	virtual s32 Behavior() override;
+	virtual s32 Render() override;
+	virtual ~Berry() override;
+	virtual u32 OnYoshiTryEat() override;
+	virtual void OnTurnIntoEgg(Player& player) override;
+	
+	void UpdateModelTransform();
+	void Kill();
+	
+	static SpawnInfo spawnData;
+	
+	static SharedFilePtr modelFile;
+	static SharedFilePtr stemModelFile;
+	
+	static u32 berryCount;
+	static u32 berryMaxCount;
+};
